7c365d0a4caf34212baab01486b5681a3722957d
CSE-18-39 <90598403+anamulshowkat@users.noreply.github.com>
2021-12-04 11:50:45 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class fibonacci{
+	public static void main(String[] args) {
+		int prev=0,current=1,upperLimit,nextTerm=0,i;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter upper limit of fibonacci series : ");
+		upperLimit = sc.nextInt();
+		String series = prev+" "+current+" ";
+		for (i=1;current+prev<upperLimit;i++) {
+			nextTerm = prev + current;
+			series= series+" "+nextTerm;
+			prev = current;
+			current = nextTerm;
+		}
+		System.out.println(series);
+	}
+}
