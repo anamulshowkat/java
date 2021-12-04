240524e427cbcb8f5e5c3934e9b6336e6bccc472
CSE-18-39 <90598403+anamulshowkat@users.noreply.github.com>
2021-12-04 11:47:53 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,24 @@
+class parent{
+	void message()
+	{
+	system.out.println("This is parent class!");
+}
+}
+class subClass extends parent
+{
+	void message()
+	{
+	system.out.println("This is child class");
+}
+}
+class demo
+{
+	public static void main(string str[])
+	{
+	parent Obj1 = new parent();
+	subClass Obj2 = new subclass();
+	Obj1.message();
+    Obj2.message1();
+    Obj3.message();
+}
+}
